02c66d376f19674f779e4c4d0c16f4728747c732
syosinsyananasi <137036997+syosinsyananasi@users.noreply.github.com>
2024-05-11 13:59:01 +0900

    Add files via upload
    weapons購入システム導入

--- RPG - Dragon Repeller/script.js
+++ RPG - Dragon Repeller/script.js
@@ -108,15 +108,22 @@ function buyHealth() {
   }
 }
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    // currentWeaponはweaponの配列の値
-    currentWeapon++;
-    goldText.innerText = gold;
-    let newWeapon = weapons[currentWeapon].name;
-    text.innerText = "You now have a " + newWeapon + ".";
+  // ？？weapons.length-1にする意味がちゃんと理解できていない
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      // currentWeaponはweaponの配列の値
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
   } else {
-    text.innerText = "You do not have enough gold to buy weapon.";
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
 // goCave内のfunction
